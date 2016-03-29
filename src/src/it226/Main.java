190846b3584a643504650113d47d7375a4d5bd36
Leyden, Ian <ileyden@ilstu.edu>
2016-03-29 12:37:16 -0500

    cleaned up code

--- src/src/it226/Main.java
+++ src/src/it226/Main.java
@@ -197,33 +197,4 @@ public class Main {
     public void deletion(Node node) {
         node.getParentNode().removeChild(node);
     }
-
-//    public class MyNodeList implements NodeList {
-//        Node root = null;
-//        int length = 0;
-//        public MyNodeList() {}
-//
-//        public void addNode(Node node) {
-//            if(root == null)
-//                root = node;
-//            else
-//                root.addChild(node);
-//            length++;
-//        }
-//
-//        public Node item(int index) {
-//            if(index < 1) return root;
-//            Node node = root;
-//            while(index > 0) {
-//                node = node.getFirstChild();
-//                if(node == null) return node;
-//                index--;
-//            }
-//            return node;
-//        }
-//
-//        public int getLength() {
-//            return length;
-//        }
-//    }
 }
